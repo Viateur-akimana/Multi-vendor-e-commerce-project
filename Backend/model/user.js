9efdf70e33b089a71bb728ba284f16ce02f6e6d0
akimana-viateur <akimanaviateur94@gmail.com>
2023-07-26 18:19:45 +0200

    Your commit message here

--- Backend/model/user.js
+++ Backend/model/user.js
@@ -1,5 +1,3 @@
-// user.js
-
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
--- Backend/multer.js
+++ Backend/multer.js
@@ -2,7 +2,7 @@ const express = require("express");
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: function (req, res, cb) {
+  destination: function (req, file, cb) {
     cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
