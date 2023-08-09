9db00f9d5a3085170e3baca5e59fc638efc8603c
akimana-viateur <akimanaviateur94@gmail.com>
2023-08-09 18:31:22 +0200

    updating some components and pages

--- fronted/src/components/Login/Login.jsx
+++ fronted/src/components/Login/Login.jsx
@@ -1,27 +1,39 @@
-import React, { useState } from "react";
+import { React, useState } from "react";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import styles from "../../styles/styles";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import server from "../../server";
+import { server } from "../../server";
+import { toast } from "react-toastify";
+
 const Login = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [visible, setVisible] = useState(false);
 
-  const handleSubmit = async(e)=>{
-    e.preventDefault()
-   await axios.post(`${server}/use/login-user`,{
-    email,
-    password
-   },{withCredentials:true }).then((res)=>{
-    toast.success("login successfully");
-    navigate("/")
-   }).catch((error)=>{
-    toast.error(error.res.data.message);
-   })
-  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios
+      .post(
+        `${server}/user/login-user`,
+        {
+          email,
+          password,
+        },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        toast.success("Login Success!");
+        navigate("/");
+        window.location.reload(true); 
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
+  };
+
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -31,7 +43,7 @@ const Login = () => {
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form className="space-y-6">
+          <form className="space-y-6" onSubmit={handleSubmit}>
             <div>
               <label
                 htmlFor="email"
@@ -109,7 +121,6 @@ const Login = () => {
             </div>
             <div>
               <button
-              onSubmit={handleSubmit}
                 type="submit"
                 className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
               >
@@ -128,4 +139,5 @@ const Login = () => {
     </div>
   );
 };
- export default Login;
+
+export default Login;
