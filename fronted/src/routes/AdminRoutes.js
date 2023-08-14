5a805e29817ce1b83106b3cc366762764adf9bda
akimana-viateur <akimanaviateur94@gmail.com>
2023-08-14 09:57:12 +0200

    all is done on some components

--- fronted/src/routes/AdminRoutes.js
+++ fronted/src/routes/AdminRoutes.js
@@ -1,10 +1,10 @@
-import AdminDashboardPage from "../pages/AdminDashboardPage";
-import AdminDashboardUsers from "../pages/AdminDashboardUsers";
-import AdminDashboardSellers from "../pages/AdminDashboardSellers";
-import AdminDashboardOrders from "../pages/AdminDashboardOrders";
-import AdminDashboardProducts from "../pages/AdminDashboardProducts";
-import AdminDashboardEvents from "../pages/AdminDashboardEvents";
-import AdminDashboardWithdraw from "../pages/AdminDashboardWithdraw";
+import AdminDashboardPage from "../pages/AdminDashboardPage.jsx";
+import AdminDashboardUsers from "../pages/AdminDashboardUsers.jsx/index.js";
+import AdminDashboardSellers from "../pages/AdminDashboardSellers.jsx/index.js";
+import AdminDashboardOrders from "../pages/AdminDashboardOrders.jsx";
+import AdminDashboardProducts from "../pages/AdminDashboardProducts.jsx/index.js";
+import AdminDashboardEvents from "../pages/AdminDashboardEvents.jsx";
+import AdminDashboardWithdraw from "../pages/AdminDashboardWithdraw.jsx/index.js";
 
 export {
   AdminDashboardPage,
