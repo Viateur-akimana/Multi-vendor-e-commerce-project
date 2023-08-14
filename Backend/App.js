4ce3832a5d7f56611669e18ab59a16e09bce3588
akimana-viateur <akimanaviateur94@gmail.com>
2023-08-14 11:07:49 +0200

    all is done on some components

--- Backend/App.js
+++ Backend/App.js
@@ -1,29 +1,54 @@
-const express = require('express');
-const ErrorHandler = require('./utils/ErrorHandler');
-const app= express();
+const express = require("express");
+const ErrorHandler = require("./middleware/error");
+const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-//middlewares
+app.use(cors({
+  origin: ['https://eshop-tutorial-pyri.vercel.app',],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(cookieParser());
-app.use("/",express.static("uploads"))
-app.use(
-    cors(
-    origin:"http://localhost:3000",
-    credentials:true
-));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use("/test", (req, res) => {
+  res.send("Hello world!");
+});
 
-if(process.env.NODE_ENV !== "PRODUCTION"){
-    require("dotenv").config({
-        path:"backend/config/.env"
-    })
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "config/.env",
+  });
 }
-//importing the rooutes
-const user= require("./controllers/user")
-app.use("api/v2",user);
-//handling errors
+
+// import routes
+const user = require("./controller/user");
+const shop = require("./controller/shop");
+const product = require("./controller/product");
+const event = require("./controller/event");
+const coupon = require("./controller/coupounCode");
+const payment = require("./controller/payment");
+const order = require("./controller/order");
+const conversation = require("./controller/conversation");
+const message = require("./controller/message");
+const withdraw = require("./controller/withdraw");
+
+app.use("/api/v2/user", user);
+app.use("/api/v2/conversation", conversation);
+app.use("/api/v2/message", message);
+app.use("/api/v2/order", order);
+app.use("/api/v2/shop", shop);
+app.use("/api/v2/product", product);
+app.use("/api/v2/event", event);
+app.use("/api/v2/coupon", coupon);
+app.use("/api/v2/payment", payment);
+app.use("/api/v2/withdraw", withdraw);
+
+// it's for ErrorHandling
 app.use(ErrorHandler);
+
 module.exports = app;
--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,43 @@
-const app = require("./App");
+const app = require("./app.");
+const connectDatabase = require("./db/Database");
+const cloudinary = require("cloudinary");
 
+// Handling uncaught Exception
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
-  console.log("Shutting down the application");
+  console.log(`shutting down the server for handling uncaught exception`);
 });
+
+// config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
-    path: "backend/config/.env",
+    path: "config/.env",
   });
 }
 
+// connect db
+connectDatabase();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+
+// create server
 const server = app.listen(process.env.PORT, () => {
-  console.log("connnected successfully");
+  console.log(
+    `Server is running on http://localhost:${process.env.PORT}`
+  );
 });
 
-process.on("unhandledRejection", () => {
-  console.log(`shutting down the server ${err.message}`);
+// unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Shutting down the server for ${err.message}`);
+  console.log(`shutting down the server for unhandle promise rejection`);
 
   server.close(() => {
-    process.exit(0);
+    process.exit(1);
   });
 });
