e4ef25b49b5d842f33008368c1e90b54aefbc6a6
akimana-viateur <akimanaviateur94@gmail.com>
2023-08-14 11:58:51 +0200

    all is done on some components

--- fronted/src/components/Home/Home.jsx
+++ fronted/src/components/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Header from "../components/Layout/Header";
+import Header from "../Layout/Header";
 import Hero from "../components/Route/Hero/Hero";
 import Categories from "../components/Route/Categories/Categories";
 import BestDeals from "../components/Route/BestDeals/BestDeals";
 import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct";
 import Events from "../components/Events/Events";
 import Sponsored from "../components/Route/Sponsored";
-import Footer from "../components/Layout/Footer";
+import Footer from "../Layout/Footer";
 
 const HomePage = () => {
   return (
--- fronted/src/components/Layout/Sponsored.jsx
+++ fronted/src/components/Layout/Sponsored.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../../../../styles/styles";
+import styles from "../../styles/styles";
 
 const Sponsored = () => {
   return (
