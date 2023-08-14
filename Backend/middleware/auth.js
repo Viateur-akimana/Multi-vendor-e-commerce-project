8ea1034566bb5402db1b7c3facbdee530a208ee2
akimana-viateur <akimanaviateur94@gmail.com>
2023-08-14 09:21:57 +0200

    all is done on some products

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,17 +1,43 @@
 const ErrorHandler = require("../utils/ErrorHandler");
-const handleAsync = require("./handleAsyncErrors");
-const jwt = require("jsonWebToken");
-const User = require("../model/user")
+const catchAsyncErrors = require("./catchAsyncErrors");
+const jwt = require("jsonwebtoken");
+const User = require("../model/user");
+const Shop = require("../model/shop");
 
-//checking for the token to login
-const isAuthenticated = async(req,res,next)=>{
-const token=req.cookie;
-if(!token){
-    return next(new ErrorHandler("please login to continue",400))
+exports.isAuthenticated = catchAsyncErrors(async(req,res,next) => {
+    const {token} = req.cookies;
+
+    if(!token){
+        return next(new ErrorHandler("Please login to continue", 401));
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    req.user = await User.findById(decoded.id);
+
+    next();
+});
+
+
+exports.isSeller = catchAsyncErrors(async(req,res,next) => {
+    const {seller_token} = req.cookies;
+    if(!seller_token){
+        return next(new ErrorHandler("Please login to continue", 401));
+    }
+
+    const decoded = jwt.verify(seller_token, process.env.JWT_SECRET_KEY);
+
+    req.seller = await Shop.findById(decoded.id);
+
+    next();
+});
+
+
+exports.isAdmin = (...roles) => {
+    return (req,res,next) => {
+        if(!roles.includes(req.user.role)){
+            return next(new ErrorHandler(`${req.user.role} can not access this resources!`))
+        };
+        next();
+    }
 }
-//verifying for the token
-const decoded=jwt.verify(token,process.env.JWT_SECRET_KEY)
-req.user=await User.findById(decoded.id)
-next()
-}
-module.exports=isAuthenticated;
--- Backend/utils/shopToken.js
+++ Backend/utils/shopToken.js
@@ -1,5 +1,5 @@
 // create token and saving that in cookies
-const sendToken = (user, statusCode, res) => {
+const sendShopToken = (user, statusCode, res) => {
     const token = user.getJwtToken();
   
     // Options for cookies
@@ -10,11 +10,11 @@ const sendToken = (user, statusCode, res) => {
       secure: true,
     };
   
-    res.status(statusCode).cookie("token", token, options).json({
+    res.status(statusCode).cookie("seller_token", token, options).json({
       success: true,
       user,
       token,
     });
   };
   
-  module.exports = sendToken;
+  module.exports = sendShopToken;
